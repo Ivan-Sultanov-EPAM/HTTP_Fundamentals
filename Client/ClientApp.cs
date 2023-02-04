0383ecfdc4c1a6fb4997416cde2bbddea064317b
Ivan Sultanov <Ivan_Sultanov@epam.com>
2023-02-04 13:08:59 +0600

    Add project files.

---
+++ Client/ClientApp.cs
@@ -0,0 +1,96 @@
+﻿namespace Client;
+
+class ClientApp
+{
+    private static HttpClient _client;
+
+    static async Task Main()
+    {
+        _client = new HttpClient();
+
+        await Act();
+    }
+
+    static async Task Act()
+    {
+        var requests = new List<string>
+        {
+            "http://localhost:8888/GetMyName/",
+            "http://localhost:8888/Information/",
+            "http://localhost:8888/Success/",
+            "http://localhost:8888/Redirection/",
+            "http://localhost:8888/ClientError/",
+            "http://localhost:8888/ServerError/",
+            "http://localhost:8888/MyNameByHeader/",
+            "http://localhost:8888/MyNameByCookies/"
+        };
+
+        while (true)
+        {
+            foreach (var request in requests)
+            {
+                var response = await _client.GetAsync(request);
+                Console.WriteLine(request);
+                Print(response);
+
+                Console.ReadKey();
+                Console.WriteLine();
+            }
+        }
+
+        static void Print(HttpResponseMessage response)
+        {
+            var text = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine($"Content: {text}");
+            Console.WriteLine($"Status Code: {response.StatusCode}");
+
+            var myNameHeader = GetHeaderValue(response, "MyNameHeader");
+
+            var myNameCookie = GetCookieValue(response, "MyNameCookie");
+
+            if (!string.IsNullOrEmpty(myNameHeader))
+            {
+                Console.WriteLine($"Headers: {myNameHeader}");
+            }
+
+            if (!string.IsNullOrEmpty(myNameCookie))
+            {
+                Console.WriteLine($"Cookie: {myNameCookie}");
+            }
+
+            Console.WriteLine();
+        }
+
+        static string GetHeaderValue(HttpResponseMessage message, string cookieName)
+        {
+            var result = message.Headers
+                .Where(h => h.Key.Equals(cookieName))
+                .Select(p => p.Value)
+                .FirstOrDefault();
+
+            if (result != null)
+            {
+                return string.Join(" ", result);
+            }
+
+            return "";
+        }
+
+        static string GetCookieValue(HttpResponseMessage message, string name)
+        {
+            var result = message.Headers
+                .Where(h => h.Key.Equals("Set-Cookie"))
+                .Select(p => p.Value)
+                .FirstOrDefault(v => v.Any(s => s.Contains("MyNameCookie")))
+                ?.FirstOrDefault();
+
+            if (result != null)
+            {
+                return result.Split('=')[1];
+            }
+
+            return "";
+        }
+    }
+}
---
+++ Listener/ListenerApp.cs
@@ -0,0 +1,91 @@
+﻿using System.Net;
+
+namespace Listener;
+
+class ListenerApp
+{
+    static readonly HttpListener _listener = new();
+
+    static async Task Main()
+    {
+        _listener.Prefixes.Add("http://localhost:8888/");
+        _listener.Start();
+        Console.WriteLine("Listening...");
+        await Listen();
+        _listener.Stop();
+    }
+
+    static async Task Listen()
+    {
+        while (true)
+        {
+            var context = await _listener.GetContextAsync();
+            var request = context.Request;
+            var response = context.Response;
+
+            var responseString = GetMyName(request);
+
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+
+            response.ContentLength64 = buffer.Length;
+
+            response.SetStatusCode(request);
+
+            var output = response.OutputStream;
+
+            await output.WriteAsync(buffer, 0, buffer.Length);
+
+            output.Close();
+        }
+    }
+
+    private static string GetMyName(HttpListenerRequest request)
+    {
+        var path = request.RawUrl?.ParseRequest();
+
+        if (path == "GetMyName")
+        {
+            return "Ivan";
+        }
+
+        return path;
+    }
+}
+
+public static class Helpers
+{
+    public static void SetStatusCode(this HttpListenerResponse response, HttpListenerRequest request)
+    {
+        switch (request.RawUrl?.ParseRequest())
+        {
+            case "Information":
+                response.StatusCode = 200;
+                break;
+            case "Success":
+                response.StatusCode = 200;
+                break;
+            case "Redirection":
+                response.StatusCode = 300;
+                break;
+            case "ClientError":
+                response.StatusCode = 400;
+                break;
+            case "ServerError":
+                response.StatusCode = 500;
+                break;
+            case "MyNameByHeader":
+                response.StatusCode = 200;
+                response.Headers.Add("MyNameHeader: Ivan");
+                break;
+            case "MyNameByCookies":
+                response.StatusCode = 200;
+                response.Cookies.Add(new Cookie("MyName", "Ivan"));
+                break;
+            default:
+                response.StatusCode = 200;
+                break;
+        }
+    }
+
+    public static string ParseRequest(this string path) => path.Trim('/');
+}
